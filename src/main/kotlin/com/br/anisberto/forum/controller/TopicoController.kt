f4ebe1b02b2d587dd214dbf4dada5033ddf2375d
anisberto reis <anisbertoos@gmail.com>
2022-05-09 22:23:10 -0300

    configuração de classe de servico e controller topico

--- src/main/kotlin/com/br/anisberto/forum/controller/TopicoController.kt
+++ src/main/kotlin/com/br/anisberto/forum/controller/TopicoController.kt
@@ -1,9 +1,23 @@
 package com.br.anisberto.forum.controller
 
+import com.br.anisberto.forum.model.Topico
+import com.br.anisberto.forum.service.TopicoService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/topicos")
-class TopicoController {
+@RequestMapping("/api/v1/topicos")
+class TopicoController(private val topicoService: TopicoService) {
+
+    @GetMapping
+    fun listar() : List<Topico>{
+        return topicoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long) : Topico{
+        return topicoService.findById(id);
+    }
 }
